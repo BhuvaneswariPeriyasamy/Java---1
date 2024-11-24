1d33c1b1c4f2151297761fa3072aab5cf988e6e8
Bhuvaneswari Periyasamy <bhuvana.irtt93@gmail.com>
2024-11-24 17:09:09 -0500

    update triangle

--- Array_SecLarge.java
+++ Array_SecLarge.java
@@ -1,8 +1,32 @@
 public class Array_SecLarge {
     public static void main(String[] args) {
 
-        int[] input = {1,2,3,5,8};
+        int[] input = {19, 23, 35, 50, 28};
+
+        int first = input[0];
+        int sec = input[1];
+        int big;
+        int secbig;
 
 
+        if (first < sec) {
+            big = sec;
+            secbig = first;
+        } else {
+            big = first;
+            secbig = sec;
+        }
+
+
+        for (int pointer = 2; pointer < input.length; pointer++) {
+            if (input[pointer] > big) {
+                secbig = big;
+                big = input[pointer];
+            } else if (input[pointer] > secbig && input[pointer] != big) {
+                secbig = input[pointer];
+            }
+        }
+
+        System.out.println("Second Largest number is: " + secbig);
     }
 }
